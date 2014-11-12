f2a0ba97a29a5ba2c7bd8aafaa778efdda8a55ca
Michal Gallovic <michalgallovic@Michals-MacBook-Pro.local>
2014-11-12 20:16:44 +0100

    fix

---
+++ app/views/ostrovfestival/index.blade.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<title>6.9.2014 Ostrov Festival Trenčín</title>
+<meta name="title" content="6.9.2014 Ostrov Festival | Trenčín" />
+<meta property="og:site_name" content="Ostrov Festival 2014" />
+<meta proprety="og:url" content="http://ostrovfestival.sk">
+<meta property="og:type" content="website" />
+<meta property="og:title" content="6.9.2014 Ostrov Festival | Trenčín" />
+<meta property="og:description" content="6.9.2014 sa uskutoční prvý ročník festivalíku spajajúceho šport, hudbu, zábavu a dobrých ľudí :)"/>
+<meta property="og:image" content="http://ostrovfestival.sk/assets/images/logo.png">
+<meta property="og:image:width" content="1024">
+<meta name="description" content="6.9.2014 sa uskutoční prvý ročník festivalíku spajajúceho šport, hudbu, zábavu a dobrých ľudí :)" />
+<link rel="icon" type="image/png" href="assets/images/favicon.ico">
+<link rel="stylesheet" href="assets/stylesheets/ostrovfestival.css">
+<link href="http://fonts.googleapis.com/css?family=Roboto&subset=latin,latin-ext" rel='stylesheet' type='text/css'>
+<link type="text/plain" rel="author" href="http://ostrovfestival.sk/humans.txt" />
+</head>
+<body>
+	<div class="scene hidden-sm">
+		<div class="text-box text-box--lineup">
+			<h2>LINE UP</h2>
+			<hr>
+			<p>12:00 - 12:45 THE 77</p>
+			<p>13:00 - 14:45 ANONYM KIDS</p>
+			<p>15:00 - 15:45 DISTANC</p>
+			<p>16:00 - 16:45 FISH FACE</p>
+			<p>17:00 - 17:45 GENUINE JACKS</p>
+			<p>18:15 - 19:00 CAPTAIN SLICE</p>
+			<p>19:30 - 20:30 VEC a ŠKRUPO</p>
+			<p>21:00 - 22:00 BILLY BARMAN</p>
+			<p>22:30 – 00:00 SLOBODNÁ EURÓPA</p>
+			<p>00:00 - AFTERKA POD VRBOU</p>
+			<p>17:00 – 18:00 VÝLETNÁ LOĎ - NO PULS</p>
+		</div>
+		<div class="text-box text-box--info">
+			<h2>INFO</h2>
+			<hr>
+			<h3>Otvorenie areálu</h3>
+			<p>6.9.2014 o 12:00</p>
+			<hr>
+			<h3>Vstupenky</h3>
+			<p>Vstupenky si môžete zakúpiť v KIC Trenčín a Šport Hotel Ostrov.</p>
+			<h3>Predpredaj</h3>
+			<p>študenti: 5€<br>dospelí: 6€</p>
+			<h3>Na mieste</h3>
+			<p>študenti: 7€<br>dospelí: 8€<br>ZŤP so sprievodom: zdarma</p>
+			<h3>Všeobecné podmienky</h3>
+			<p>Zmena programu vyhradená. Zaplatené vstupné sa nevracia, vstupenka je nenávratná a nevymieňa sa. </p>
+			<span class="readmore">Organizátor si vyhradzuje...</span>
+	        <div class="message">
+	          Organizátor si vyhradzuje právo odoprieť vstup a v prípade takéhoto odopretia preplatiť cenu vstupenky. Vstupenka je pri vstupe nahradená označením návštevníka festivalu náramkom. Za stratu, odcudzenie alebo poškodenie označenia nenesie organizátor zodpovednosť. Organizátor nenesie zodpovednosť za osobné škody, stratu a zranenia vzniknuté na podujatí, ak boli spôsobené nedbalosťou držiteľa lístka alebo nepredvídateľným správaním iných návštevníkov a tretích osôb. Vstup na festivalové pódium je pre návštevníkov zakázaný, porušenie zákazu je dôvodom na vyvedenie z areálu a odobratie identifikačného náramku. Skákanie z pódia do davu, tzv. stage diving, je na festivale zakázaný, porušenie zákazu je dôvodom na vyvedenie z areálu a odobratie identifikačného náramku. Oprávneným vstupom do areálu návštevník poskytuje svoj výslovný súhlas s tým, že fotografie, filmy alebo videozáznamy s jeho osobou, ktoré sú zhotovené počas festivalu alebo v súvislosti s ním, môžu byť použité prostredníctvom akejkoľvek súčasnej alebo budúcej technickej metódy, a to bez peňažnej náhrady a časového obmedzenia. Je prísne zakázané vnášať do priestoru konania festivalu sklenené nádoby, nádoby pod tlakom, pyrotechnické predmety, sklenené fľaše, dáždniky, predmety ohrozujúce bezpečnosť návštevníkov, zbrane akéhokoľvek druhu a zakladať otvorený oheň. Osoby vstupujúce do areálu môžu byť prehľadané. 
+	        </div>
+			
+		</div>
+		<div class="hill-movable">
+			<img src="assets/images/hill-full.png" alt="">
+			<div class="zlobor"><img src="assets/images/zlobor.png" alt=""></div>
+		</div>
+		<div class="hill">
+			<img id="magnificent-hill" src="assets/images/hill-full.png" alt="">
+			<div class="zlobor"><img src="assets/images/zlobor.png" alt=""></div>
+			<div id="logo" class="logo rrrr"><a href="#"><img src="assets/images/logo.png" alt=""></a></div>
+			<div class="billy rrrr"><a href="http://www.billybarman.sk/" target="_blank"><img src="assets/images/billy.png" alt=""></a></div>
+			<div class="captain rrrr"><a href="http://www.captainslice.lubor.sk/" target="_blank"><img src="assets/images/captain.png" alt=""></a></div>
+			<div id="menu" class="menu">
+				<div class="mapa"><a href="assets/pdf/mapa.pdf" target="_blank"><img src="assets/images/mapa.png" alt=""></a></div>
+				<div class="lineup"><a href="#"><img src="assets/images/lineup.png" alt=""></a></div>
+				<div class="info"><a href="#"><img src="assets/images/info.png" alt=""></a></div>
+				<div class="facebook"><a href="https://www.facebook.com/events/1478481899065733/" target="_blank"><img src="assets/images/facebook.png" alt=""></a></div>
+			</div>
+			<div class="distanc rrrr"><a href="http://bandzone.cz/distanctrenchtown" target="_blank"><img src="assets/images/distance.png" alt=""></a></div>
+			<div class="genuine rrrr"><a href="http://bandzone.cz/genuinejacks" target="_blank"><img src="assets/images/genuine.png" alt=""></a></div>
+			<div class="anonymkids rrrr"><a href="http://bandzone.cz/anonymkids" target="_blank"><img src="assets/images/anonymkids.png" alt=""></a></div>
+			<div class="vstup rrrr"><img src="assets/images/vstup.png" alt=""></div>
+			<div id="lodicka-container" class="lodicka-container">
+				<a href="https://www.youtube.com/watch?v=-y3mYY_2GIs" target="_blank">
+					<div class="lodicka">
+						<img src="assets/images/lodicka.png" alt="">
+						<div class="nopuls"><img src="assets/images/nopuls.png" alt=""></div>
+					</div>
+				</a>
+			</div>
+			<div class="kedykde rrrr"><img src="assets/images/kedykde.png" alt=""></div>
+			<div class="aktivity rrrr"><img src="assets/images/aktivity.png" alt=""></div>
+			<div class="slobodna rrrr"><a href="http://www.slobodnaeuropa.sk/" target="_blank"><img class="img-responsive" src="assets/images/slobodna.png" alt=""></a></div>
+			<div class="vec rrrr"><a href="http://brancikovac.bandcamp.com/" target="_blank"><img src="assets/images/vec.png" alt=""></a></div>
+			<div class="the77 rrrr"><a href="http://bandzone.cz/the77" target="_blank"><img src="assets/images/the77.png" alt=""></a></div>
+			<div class="clouds oblak-zlobor" id=""><img src="assets/images/oblak1.png" alt=""></div>
+			<div class="clouds oblak-billy"><img src="assets/images/oblak2.png" alt=""></div>
+			<div class="clouds oblak-captain"><img src="assets/images/oblak3.png" alt=""></div>
+			<div class="clouds oblak-x"><img src="assets/images/oblak4.png" alt=""></div>
+			<div class="clouds oblak-vec"><img src="assets/images/oblak5.png" alt=""></div>
+			<div class="clouds oblak-genuine"><img src="assets/images/oblak6.png" alt=""></div>
+			<div class="clouds oblak-anonymkids"><img src="assets/images/oblak7.png" alt=""></div>
+			<div class="clouds oblak-slobodna"><img src="assets/images/oblak8.png" alt=""></div>
+			<div class="clouds oblak-kedykde"><img src="assets/images/oblak9.png" alt=""></div>
+			<div class="cervik"><img src="assets/images/cervik.png" alt=""></div>
+			<div class="ryba"><img src="assets/images/ryba.png" alt=""></div>
+		</div>
+		
+	</div>
+	
+	
+	<div class="wave-back hidden-xs hidden-sm">
+	</div>
+	<div class="wave-front hidden-xs hidden-sm">
+	</div>
+	<div class="wave-bubliny hidden-xs hidden-sm">
+	</div>
+	<div class="slnko hidden-xs hidden-sm">
+		<img src="assets/images/slnko.png" alt="">
+	</div>
+	<div class="container visible-xs visible-sm">
+		<nav class="navbar navbar-default" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#"><img class="mobile-logo" src="assets/images/logo.png" alt=""></a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li><a href="assets/pdf/mapa.pdf">MAPA</a></li>
+		        <li><a id="mob-lineup" href="#">LINE UP</a></li>
+		        <li><a id="mob-info" href="#">INFO</a></li>
+		        <li><a href="https://www.facebook.com/events/1478481899065733/">FACEBOOK</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+	<div class="container visible-xs visible-sm" style="color:white;">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="text-center">
+					<h1>6.9 2014 | Trenčí­n</h1>
+					<h2>AREÁL HOTELA OSTROV</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+				<div class="col-xs-12">
+					<h3>Vystúpia:</h3>
+				</div>
+
+		</div>
+	</div>
+	<div class="container visible-sm artists-list">
+		<a href="http://www.billybarman.sk/" target="_blank">
+			<div class="artists-mob billy-mob">
+				Billy Barman
+			</div>
+		</a>
+		<a href="http://www.slobodnaeuropa.sk/" target="_blank">
+			<div class="artists-mob slobodna-mob">
+				Slobodná Európa
+			</div>
+		</a>
+		<a href="http://brancikovac.bandcamp.com/" target="_blank">
+			<div class="artists-mob vec-mob">
+				Vec &amp; Šrupo
+			</div>
+		</a>
+		<a href="https://www.youtube.com/watch?v=-y3mYY_2GIs" target="_blank">
+			<div class="artists-mob nopuls-mob">
+				No Puls
+			</div>
+		</a>
+		<a href="http://www.captainslice.lubor.sk/" target="_blank">
+			<div class="artists-mob captain-mob">
+				Captain Slice
+			</div>
+		</a>
+		<a href="http://bandzone.cz/distanctrenchtown" target="_blank">
+			<div class="artists-mob distanc-mob">
+				Distanc
+			</div>
+		</a>
+		<a href="http://bandzone.cz/the77" target="_blank">
+			<div class="artists-mob the77-mob">
+				The 77
+			</div>
+		</a>
+		<a href="http://bandzone.cz/genuinejacks" target="_blank">
+			<div class="artists-mob genuine-mob">
+				Genuine Jacks
+			</div>
+		</a>
+		<a href="http://bandzone.cz/anonymkids" target="_blank">
+			<div class="artists-mob anonymkids-mob">
+				Anonym Kids
+			</div>
+		</a>
+	</div>
+	
+	<div class="visible-xs artists-list">
+		<a href="http://www.billybarman.sk/" target="_blank">
+			<div class="artists-mob billy-mob">
+				Billy Barman
+			</div>
+		</a>
+		<a href="http://www.slobodnaeuropa.sk/" target="_blank">
+			<div class="artists-mob slobodna-mob">
+				Slobodná Európa
+			</div>
+		</a>
+		<a href="http://brancikovac.bandcamp.com/" target="_blank">
+			<div class="artists-mob vec-mob">
+				Vec &amp; Škrupo
+			</div>
+		</a>
+		<a href="https://www.youtube.com/watch?v=-y3mYY_2GIs" target="_blank">
+			<div class="artists-mob nopuls-mob">
+				No Puls
+			</div>
+		</a>
+		<a href="http://www.captainslice.lubor.sk/" target="_blank">
+			<div class="artists-mob captain-mob">
+				Captain Slice
+			</div>
+		</a>
+		<a href="http://bandzone.cz/distanctrenchtown" target="_blank">
+			<div class="artists-mob distanc-mob">
+				Distanc
+			</div>
+		</a>
+		<a href="http://bandzone.cz/the77" target="_blank">
+			<div class="artists-mob the77-mob">
+				The 77
+			</div>
+		</a>
+		<a href="http://bandzone.cz/genuinejacks" target="_blank">
+			<div class="artists-mob genuine-mob">
+				Genuine Jacks
+			</div>
+		</a>
+		<a href="http://bandzone.cz/anonymkids" target="_blank">
+			<div class="artists-mob anonymkids-mob">
+				Anonym Kids
+			</div>
+		</a>
+	</div>
+	<div class="info-mob visible-xs visible-sm" style="z-i">
+		<div id="scroll-to-lineup" class="container">
+			<h1>LINE UP</h1>
+			<div class="row">
+				<div class="col-xs-12">
+					<p>12:00 - 12:45 THE 77</p>
+					<p>13:00 - 14:45 ANONYM KIDS</p>
+					<p>15:00 - 15:45 DISTANC</p>
+					<p>16:00 - 16:45 FISH FACE</p>
+					<p>17:00 - 17:45 GENUINE JACKS</p>
+					<p>18:15 - 19:00 CAPTAIN SLICE</p>
+					<p>19:30 - 20:30 VEC a ŠKRUPO</p>
+					<p>21:00 - 22:00 BILLY BARMAN</p>
+					<p>22:30 – 00:00 SLOBODNÁ EURÓPA</p>
+					<p>00:00 - AFTERKA POD VRBOU</p>
+					<p>17:00 – 18:00 VÝLETNÁ LOĎ - NO PULS</p>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<div id="scroll-to-info" class="container">
+			<h1>INFO</h1>
+			<div class="row">
+				<div class="col-xs-12">
+					<h2>INFO</h2>
+	        		<hr>
+	        		<h3>Otvorenie areálu</h3>
+	        		<p>6.9.2014 o 12:00</p>
+	        		<hr>
+	        		<h3>Vstupenky</h3>
+	        		<p>Vstupenky si môžete zakúpiť v KIC Trenčín a Šport Hotel Ostrov.</p>
+	        		<h3>Predpredaj</h3>
+	        		<p>študenti: 5€<br>dospelí: 6€</p>
+	        		<h3>Na mieste</h3>
+	        		<p>študenti: 7€<br>dospelí: 8€<br>ZŤP so sprievodom: zdarma</p>
+	        		<h3>Všeobecné podmienky</h3>
+	        		<p>Zmena programu vyhradená. Zaplatené vstupné sa nevracia, vstupenka je nenávratná a nevymieňa sa. </p>
+	        		<span class="readmore" style="cursor: pointer; text-decoration:underline;">Organizátor si vyhradzuje...</span>
+	                <div class="message">
+	                  Organizátor si vyhradzuje právo odoprieť vstup a v prípade takéhoto odopretia preplatiť cenu vstupenky. Vstupenka je pri vstupe nahradená označením návštevníka festivalu náramkom. Za stratu, odcudzenie alebo poškodenie označenia nenesie organizátor zodpovednosť. Organizátor nenesie zodpovednosť za osobné škody, stratu a zranenia vzniknuté na podujatí, ak boli spôsobené nedbalosťou držiteľa lístka alebo nepredvídateľným správaním iných návštevníkov a tretích osôb. Vstup na festivalové pódium je pre návštevníkov zakázaný, porušenie zákazu je dôvodom na vyvedenie z areálu a odobratie identifikačného náramku. Skákanie z pódia do davu, tzv. stage diving, je na festivale zakázaný, porušenie zákazu je dôvodom na vyvedenie z areálu a odobratie identifikačného náramku. Oprávneným vstupom do areálu návštevník poskytuje svoj výslovný súhlas s tým, že fotografie, filmy alebo videozáznamy s jeho osobou, ktoré sú zhotovené počas festivalu alebo v súvislosti s ním, môžu byť použité prostredníctvom akejkoľvek súčasnej alebo budúcej technickej metódy, a to bez peňažnej náhrady a časového obmedzenia. Je prísne zakázané vnášať do priestoru konania festivalu sklenené nádoby, nádoby pod tlakom, pyrotechnické predmety, sklenené fľaše, dáždniky, predmety ohrozujúce bezpečnosť návštevníkov, zbrane akéhokoľvek druhu a zakladať otvorený oheň. Osoby vstupujúce do areálu môžu byť prehľadané. 
+	                </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="noise"></div>
+
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="assets/javascripts/ostrovfestival.js"></script>
+</body>
+</html>	
